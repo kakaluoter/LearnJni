ab1c1f296e42a81b257b72e5dcc3d3785db2b283
yuanshan <xdqh2016@163.com>
2018-11-05 13:53:22 +0800

    success

--- app/src/main/java/com/example/ys/learnjni/MainActivity.java
+++ app/src/main/java/com/example/ys/learnjni/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ys.learnjni;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.show);
-        textView.setText(get());
+        textView.setText(get()+"");
 
+    }
+    public String JavaGet(int a,String str)
+    {
+        Log.v("ysysys",a+str);
+        return "i from java function!+a+str"+a+str;
     }
     public native String get();
     public native void set(String str);
--- jni/test.cpp
+++ jni/test.cpp
@@ -5,10 +5,23 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+	jstring callfromjava(JNIEnv *env,jobject thiz)
+	{
+		//jclass clazz = env->FindClass("com/example/ys/learnjni/MainActivity");
+		jclass clazz = env->GetObjectClass(thiz);
+		if(clazz==NULL)return NULL;
+		jmethodID id = env->GetMethodID(clazz,"JavaGet","(ILjava/lang/String;)Ljava/lang/String;");
+		jstring string = env->NewStringUTF(" ys ");
+		jint mint =23;
+		jstring str =(jstring)env->CallObjectMethod(thiz,id,mint,string);
+		return str;
+		
+	}
+
 	jstring Java_com_example_ys_learnjni_MainActivity_get(JNIEnv *env,jobject thiz)
 	{
 		printf("hello from android app c++ in jni\n");
-		return env->NewStringUTF("hello world from jni!");
+		return callfromjava(env,thiz);
 	}
 
 	void Java_com_example_ys_learnjni_MainActivity_set(JNIEnv *env,jobject thiz,jstring string)
